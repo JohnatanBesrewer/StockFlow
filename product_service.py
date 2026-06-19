7878c8af7836293b48100f99d6ab3c3e9bcfdde0
JohnatanBesrever <johnatanbesrewer@gmail.com>
2026-06-19 17:54:37 +0300

    update

---
+++ product_service.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+class ProductService:
+    def create_product(self, name, price, quantity):
+        pass
+
+    def get_product(self, product_id):
+        pass
+
+    def get_all_products(self):
+        pass
+
+    def update_product(self, product_id, **data):
+        pass
+
+    def delete_product(self, product_id):
+        pass
+
+    def increase_stock(self, product_id, amount):
+        pass
+
+    def decrease_stock(self, product_id, amount):
+        pass
+
+    def search_products(self, query):
+        pass
